d6249dfc9b15d69cd11ad2f1cc0c90d2093aba4c
andreicismas <andrei_cismas@yahoo.it>
2021-04-29 21:03:42 +0200

    begin_vue_first_project

---
+++ script.js
@@ -0,0 +1,23 @@
+
+
+
+// Descrizione:
+// Stampare a schermo un messaggio all’interno di un h1, utilizzando i data.
+
+
+// Bonus:
+// Aggiungere alla pagina un’immagine, presa anch’essa da un data.
+
+
+const app = new Vue(
+    {
+        el: '#contenitore',
+        data:
+        {
+            nome: "",
+            cognome: "",
+        }
+       
+
+    }
+)
